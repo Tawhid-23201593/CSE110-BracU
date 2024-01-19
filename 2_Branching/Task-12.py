b8947a7f29c0cb12f2647e4e2806d586fc081039
Tawhid-23201593 <tawhid.hasan@g.bracu.ac.bd>
2024-01-19 23:21:21 +0600

    12

---
+++ 2_Branching/Task-12.py
@@ -0,0 +1,13 @@
+t = int(input('Time: '))
+if 4<=t>=6:
+    print("Breakfast")
+elif 12<=t>=13:
+    print("Lunch")
+elif 16<=t>=17:
+    print("Snacks")
+elif 19<=t>=20:
+    print("Dinner")
+elif 1<=t>=24:
+    print("Patience is a vartue")
+else:
+    print("Wrong Time")

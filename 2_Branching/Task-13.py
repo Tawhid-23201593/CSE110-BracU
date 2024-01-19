7614bd445b612cde08f2a3f6313561bfb144840c
Tawhid-23201593 <tawhid.hasan@g.bracu.ac.bd>
2024-01-19 23:26:20 +0600

    13

--- 2_Branching/Task-13.py
+++ 2_Branching/Task-13.py
@@ -1,1 +1,13 @@
-ss  ww
+m = int(input("Marks: "))
+if m>=90:
+    print("A")
+elif 79<m<90:
+    print("B")
+elif 69<m<80:
+    print("C")
+elif 59<m<70:
+    print("D")
+elif 49<m<60:
+    print("E")
+else:
+    print("F")

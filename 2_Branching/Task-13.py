79bc811cd08bc7b3e76f82fcd1724e6e9f8170e8
Tawhid-23201593 <tawhid.hasan@g.bracu.ac.bd>
2024-01-19 23:47:40 +0600

    ques added of task 13

--- 2_Branching/Task-13.py
+++ 2_Branching/Task-13.py
@@ -1,3 +1,17 @@
+"""
+Write the Python code of a program that reads a student’s mark for
+a single subject, and prints out the corresponding grade for that mark.
+The mark ranges and corresponding grades are shown in the table below.
+You need to make sure that the mark is valid. For example, a student cannot
+receive -5 or 110 marks. So, the valid marks range from 0 to 100.
+Marks       	Grade
+90 or above	      A
+80-89	          B
+70-79	          C
+60-69	          D
+50-59	          E
+Below 50	      F
+"""
 m = int(input("Marks: "))
 if m>=90:
     print("A")

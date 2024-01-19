e42bb5e052ba383c406dd66bbcfbce37d1060986
Tawhid-23201593 <tawhid.hasan@g.bracu.ac.bd>
2024-01-19 23:53:21 +0600

    15 ques

--- 2_Branching/Task-15.py
+++ 2_Branching/Task-15.py
@@ -1,3 +1,29 @@
+"""
+Write a python program that takes the CGPA and no of credits completed by a student
+and prints whether the student is eligible for a waiver and of what percentage.
+To be eligible for a waiver, a student must have completed at least 30 credits
+and earned a CGPA greater or equal to 3.8. If not, please print "The student is not
+eligible for a waiver".
+
+CGPA	         Waiver percentage
+3.80 - 3.89	        5 percent
+3.90 - 3.94	        50 percent
+3.95 - 3.99	        75 percent
+4.00	            100 percent
+==========================================================
+Sample Input 1:
+3.93
+78
+Sample Output 1:
+The student is eligible for a waiver of 50 percent.
+==========================================================
+Sample Input 2:
+3.79
+24
+Sample Output 2:
+The student is not eligible for a waiver
+==========================================================
+"""
 c = int(input("Creadit compeated: "))
 cgpa =float(input("CGPA: "))
 if c > 29:
--- 2_Branching/task-16.py
+++ 2_Branching/task-16.py
@@ -1,3 +1,4 @@
+
 n = int (input(" Give a number: "))
 if n%2==0 or n%5==0:
     print("No")
